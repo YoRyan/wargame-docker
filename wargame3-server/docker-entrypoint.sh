3aedcfaf2540f875e740b258bae9e287b8215bb4
Ryan A Young <rayoung@utexas.edu>
2016-08-08 13:31:58 -0500

    Don't hardcode a port since this prevents us from running multiple servers.

--- wargame3-server/docker-entrypoint.sh
+++ wargame3-server/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'wargame' ]; then
     echo "login=\"$login\"" >login.ini
     echo "dedicated_key=\"$dedicated_key\"" >>login.ini
     shift
-    exec ./wargame3-server +port 10001 +port_mms 10804 +ip "$ip" $@
+    exec ./wargame3-server +port_mms 10804 +ip "$ip" +port "$port" $@
 fi
 
 exec "$@"
